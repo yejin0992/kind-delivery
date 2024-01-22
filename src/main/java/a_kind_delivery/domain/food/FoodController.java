17d447fc8fd2530eed78ee65aa0e18b87658eb5f
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2024-01-22 23:16:15 +0900

    <feat> 음식 전체조회, 개별 수정, 개별 삭제 기능 추가

--- src/main/java/a_kind_delivery/domain/food/FoodController.java
+++ src/main/java/a_kind_delivery/domain/food/FoodController.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Map;
+
 
 @Controller
 @ResponseBody // 클라한테 돌려줄때 데이터형식으로 보내줌
@@ -12,13 +17,35 @@ public class FoodController {
     @Autowired
     private FoodService foodService;
 
-    @GetMapping("/findFood/{key}")
+    // 음식 찾기
+    @RequestMapping(value = "/findFood/{key}", method= RequestMethod.GET)
     public Food findFoodByKey(@PathVariable("key") int key){
         return foodService.findFood(key);
     }
 
-    @PostMapping("/saveFood")
+    // 음식 저장
+    @RequestMapping(value="/saveFood", method= RequestMethod.POST)
     public void insertFood(@RequestBody Food food) {
         foodService.saveFood(food);
     }
+
+    // 음식 전체 조회
+    @RequestMapping(value = "/findAllFood", method=RequestMethod.GET)
+    public java.util.Map<Integer,Food> findAllFood(){
+        return foodService.findAllFood();
+    }
+
+    // 음식 개별 수정
+    @RequestMapping(value="/updateFood/{key}", method = RequestMethod.PATCH)
+    public Food updateFood(@PathVariable("key") int key, @RequestBody Food food){
+        return foodService.updateFood(key,food);
+    }
+
+    // 음식 개별 삭제
+    @RequestMapping(value="/deleteFood/{key}", method= RequestMethod.DELETE)
+    public void deleteFood(@PathVariable("key") int key){
+        foodService.deleteFood(key);
+    }
 }
+
+
--- src/main/java/a_kind_delivery/domain/food/FoodService.java
+++ src/main/java/a_kind_delivery/domain/food/FoodService.java
@@ -3,18 +3,39 @@ package a_kind_delivery.domain.food;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Map;
+
 @Service
 public class FoodService {
 
     @Autowired
     private FoodRepository foodRepository;
 
+    //음식 찾기
     public Food findFood(int key){
         return foodRepository.findFood(key);
     }
 
+    //음식 저장
     public void saveFood(Food food){
         foodRepository.saveFood(food);
     }
 
+    //음식 전체 조회
+    public java.util.Map<Integer, Food> findAllFood(){
+        return foodRepository.findAllFood();
+    }
+
+    //음식 개별 수정
+    public Food updateFood(int key, Food food){
+        return foodRepository.updateFood(key, food);
+    }
+
+    //음식 개별 삭제
+    public void deleteFood(int key){
+        foodRepository.deleteFood(key);
+    }
 }
