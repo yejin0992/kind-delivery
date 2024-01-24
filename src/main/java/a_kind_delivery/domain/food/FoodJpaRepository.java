6b01f0e3144dd94f63f4d8931427ac6460c254c4
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2024-01-25 01:37:49 +0900

    <feat> JpaRepository 인터페이스 추가

--- src/main/java/a_kind_delivery/domain/food/FoodJpaRepository.java
+++ src/main/java/a_kind_delivery/domain/food/FoodJpaRepository.java
@@ -1,2 +1,27 @@
-package a_kind_delivery.domain.food;public interface FoodJapRepository {
+package a_kind_delivery.domain.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// JpaRepository는 @NoRepositoryBean 상속받고 있음
+// 빈으로 직접 등록되지 않지만 SpringDataJPA가 직접 구현하여 bean 생성함
+public interface FoodJpaRepository extends JpaRepository<Food, String> {
+
+//음식 찾기
+    @Override
+    <S extends Food> S save(S Food);
+
+    @Override
+    Food getById(String id);
+
+    @Override
+    List<Food> findAll();
+
+    @Override
+    void deleteById(String id);
 }
+
+
+
