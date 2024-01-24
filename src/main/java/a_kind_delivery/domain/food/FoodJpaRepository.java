1185e86e8b2eadb5b8f7875e8b01211c1c80f516
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2024-01-25 01:30:09 +0900

    <chore> build dependency(jpa,mariadb) 추가

---
+++ src/main/java/a_kind_delivery/domain/food/FoodJpaRepository.java
@@ -0,0 +1,2 @@
+package a_kind_delivery.domain.food;public interface FoodJapRepository {
+}
--- src/main/java/a_kind_delivery/domain/food/FoodMapRepository.java
+++ src/main/java/a_kind_delivery/domain/food/FoodMapRepository.java
@@ -1,8 +1,8 @@
 package a_kind_delivery.domain.food;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
